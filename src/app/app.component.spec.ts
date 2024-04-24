ed46e41a9eefad2613b21a0d8015c769253b1cc1
Brandon Mullins <webdevmullins@gmail.com>
2024-04-23 19:08:22 -0500

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,19 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { beforeEach, describe, expect, it } from 'vitest'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, AppComponent],
-    }).compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [RouterTestingModule, AppComponent]
+		}).compileComponents()
+	})
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-});
+	it('should create the app', () => {
+		const fixture = TestBed.createComponent(AppComponent)
+		const app = fixture.componentInstance
+		expect(app).toBeTruthy()
+	})
+})
--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,16 +1,16 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core'
 import {
-  provideServerRendering,
-  ɵSERVER_CONTEXT as SERVER_CONTEXT,
-} from '@angular/platform-server';
+	provideServerRendering,
+	ɵSERVER_CONTEXT as SERVER_CONTEXT
+} from '@angular/platform-server'
 
-import { appConfig } from './app.config';
+import { appConfig } from './app.config'
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    { provide: SERVER_CONTEXT, useValue: 'ssr-analog' },
-  ],
-};
+	providers: [
+		provideServerRendering(),
+		{ provide: SERVER_CONTEXT, useValue: 'ssr-analog' }
+	]
+}
 
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+export const config = mergeApplicationConfig(appConfig, serverConfig)
